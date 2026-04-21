6284ab0c5567d5516043d8cb5c0399036ba53257
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-04-21 23:46:41 +0100

    float4 optimization

--- reduction/elemwise_f32x4.cu
+++ reduction/elemwise_f32x4.cu
@@ -31,6 +31,11 @@ int main() {
     std::vector<float> h_b(N);
     std::vector<float> h_c(N);
 
+    for (int i = 0; i < N; i++) {
+        h_a = rand() / (float)RAND_MAX;
+        h_b = rand() / (float)RAND_MAX;
+    }
+
     float *d_a = nullptr;
     float *d_b = nullptr;
     float *d_c = nullptr;
@@ -38,7 +43,38 @@ int main() {
     size_t size = N * sizeof(float);
 
     int tpB = 256/4;
-    int bpG = N/256;
+    int bpG = (N / 256 - 1) / 256;
+
+    cudaMalloc(&d_a, size);
+    cudaMalloc(&d_b, size);
+    cudaMalloc(&d_c, size);
+
+    cudaMemcpy(d_a, h_a.data(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b.data(), size, cudaMemcpyHostToDevice);
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    elemwise_f32x4<<<bpG, tpB>>>(d_a, d_b, d_c, N);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    cudaMemcpy(h_c.data(), d_c, size, cudaMemcpyDeviceToHost);
+
+    float gpuDuration = 0.0f;
+
+    cudaEventElapsedTime(&gpuDuration, start, stop);
+
+    std::cout << gpuDuration << " ms" << std::endl;
+
+
+   cudaFree(d_a);
+   cudaFree(d_b);
+   cudaFree(d_c);
+
+   return 0;
 
 
 }
