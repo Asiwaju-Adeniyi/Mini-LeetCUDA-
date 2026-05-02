cd2a5356cc7bcedc6e63916d6203a01dc25f12f7
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-05-02 18:39:21 +0100

    more tweaks for the safe softmax

--- softmax/safe.cu
+++ softmax/safe.cu
@@ -11,3 +11,15 @@
 #define WarpSize 32
 #define FLOAT4(value)(<reinterpret_cast<float4 *> (&(value))[0])
 
+template <const int NumThreads = 256> 
+__global__ void safe_softmax_f32(float *a, float *b, int N) {
+    const int idx = threadIdx.x + blockIdx.x + blockDim.x;
+
+    float val = (idx < N) ? a[idx] : -FLT_MAX;
+    float max_val = block_reduction_max<NumThreads>(val);
+
+    float regA = (idx < N) ? __expf(val[idx] - max_val) : 0.0f;
+    float regA_exp = block_reduction_sum<NumThreads>(regA);
+
+    b[idx] = regA / regA_exp;
+}
